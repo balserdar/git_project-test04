10fabe4868b1c3e04130b182b7fc48dc60ec5942
Serdar <balserdar@gmail.com>
2020-03-16 18:29:54 -0500

    First commit from local master

---
+++ src/git_day3/Gitday3.java
@@ -0,0 +1,20 @@
+package git_day3;
+
+public class Gitday3 {
+	
+	
+	
+
+	public static void main(String[] args) {
+		
+		System.out.println("local first commit master branch");
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
